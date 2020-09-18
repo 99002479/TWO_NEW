5e2bc2d6f804381fe82738a0db2383fad9f6f5b8
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-19 01:23:04 +0530

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -1,16 +1,227 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "sum.h"					/* Add include file*/
-#include "test_sum.h"				/* Add include file*/
-int main()
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ void input();
+ void writefile();
+ void search();
+ void output();
+
+ struct date{
+	   int month;
+	   int day;
+	   int year;
+	   };
+
+  struct account {
+	int number;
+	char name[100];
+	int acct_no;
+	float mobile_no;
+	char street[100];
+	char city[100];
+	char acct_type;
+	float oldbalance;
+	float newbalance;
+	float payment;
+	struct date lastpayment;
+  }customer;
+  int tl,sl,ts;
+
+ void main()
 	{
-    	printf("Hello world!\n");
+	  int i,n;
+	  char ch;
 
-		int my_sum = sum(10, 20);					/* Add function call */
-		printf("Sum = %d", my_sum);
+	  printf("   CUSTOMER BILLING SYSTEM:\n\n");
+	  printf("===============================\n");
+	  printf("\n1:    ADD ACOOUNT\n");
+	  printf("2:    SEARCH CUSTOMER\n");
+	  printf("3:    EXIT\n");
+	  printf("\n================================\n");
 
-		/* Calling test main*/
-    	test_main();								/* Add function call */
+	  do{
+	       printf("\nOPTION:");
+	       ch=getche();
+	  }while(ch<='0' || ch>'3');
 
-		return 0;
-	}
+	  switch(ch){
+		case '1':
+
+			printf("\nHOW MANY ACCOUNTS?");
+			scanf("%d",&n);
+			for(i=0;i<n;i++){
+				input();
+				if(customer.payment>0)
+					customer.acct_type=(customer.payment > customer.oldbalance)? 'O': 'D';
+				else
+					customer.acct_type=(customer.oldbalance>0)?'D' : 'C';
+				customer.newbalance=customer.oldbalance - customer.payment;
+				writefile();
+			}
+			main();
+		case '2':
+
+			printf("SEARCH BY?\n");
+			printf("\n1 --- Search by customer number\n");
+			printf("2 --- Search by customer name\n");
+			search();
+			ch=getche();
+			main();
+		case '3':
+			exit(1);
+	  }
+ }
+
+
+   void input()
+	{
+	  FILE *fp=fopen("Record.dat","rb");
+	  fseek (fp,0,SEEK_END);
+	  tl=ftell(fp);
+	  sl=sizeof(customer);
+	  ts=tl/sl;
+	  fseek(fp,(ts-1)*sl,SEEK_SET);
+	  fread(&customer,sizeof(customer),1,fp);
+	  printf("\nCustomer no:%d\n",++customer.number);
+	  fclose(fp);
+	  printf("         Account number:");
+	  scanf("%d",&customer.acct_no);
+	  printf("\n       Name:");
+	  scanf("%s",customer.name);
+	  printf("\n       mobile no:");
+	  scanf("%f",&customer.mobile_no);
+	  printf("         Street:");
+	  scanf("%s",customer.street);
+	  printf("         City:");
+	  scanf("%s",customer.city);
+	  printf("         Previous balance:");
+	  scanf("%f",&customer.oldbalance);
+	  printf("         Current payment:");
+	  scanf("%f",&customer.payment);
+	  printf("         Payment date(mm/dd/yyyy):");
+	  scanf("%d/%d/%d",&customer.lastpayment.month,&customer.lastpayment.day,&customer.lastpayment.year);
+	  return;
+   }
+
+   void writefile()
+   {
+	  FILE *fp;
+	  fp=fopen("Record.dat","ab");
+	  fwrite(&customer,sizeof(customer),1,fp);
+	  fclose(fp);
+	  return;
+   }
+
+   void search()
+   {
+	 char ch;
+	 char nam[100];
+	 int n,i,m=1;
+	 FILE *fp;
+	 fp=fopen("Record.dat","rb");
+
+	 do{
+		printf("\nEnter your choice:");
+		ch=getche();
+	 }while(ch!='1' && ch!='2');
+
+	 switch(ch){
+	      case '1':
+		    fseek(fp,0,SEEK_END);
+		    tl=ftell(fp);
+		    sl=sizeof(customer);
+		    ts=tl/sl;
+		    do{
+			printf("\nChoose customer number:");
+			scanf("%d",&n);
+			if(n<=0 || n>ts)
+			printf("\nEnter valid number\n");
+			else{
+			    fseek(fp,(n-1)*sl,SEEK_SET);
+			    fread(&customer,sl,1,fp);
+			    output();
+			}
+			printf("\n\nAgain?(y/n)");
+			ch=getche();
+		    }while(ch=='y');
+		    fclose(fp);
+		    break;
+	      case '2':
+		    fseek(fp,0,SEEK_END);
+		    tl=ftell(fp);
+		    sl=sizeof(customer);
+		    ts=tl/sl;
+		    fseek(fp,(ts-1)*sl,SEEK_SET);
+		    fread(&customer,sizeof(customer),1,fp);
+		    n=customer.number;
+
+		    do{
+			printf("\nEnter the name:");
+			scanf("%s",nam);
+			fseek(fp,0,SEEK_SET);
+			for(i=1;i<=n;i++)
+			{
+			     fread(&customer,sizeof(customer),1,fp);
+			     if(strcmp(customer.name,nam)==0)
+			     {
+				output();
+				m=0;
+				break;
+			     }
+			}
+			if(m!=0)
+			printf("\n\nDoesn't exist\n");
+			printf("\nAnother?(y/n)");
+			ch=getche();
+		    }while(ch=='y');
+		    fclose(fp);
+	      }
+	      return;
+	 }
+
+
+
+   void output()
+	 {
+	   printf("\n\n    Customer no    :%d\n",customer.number);
+	   printf("    Name 	   :%s\n",customer.name);
+	   printf("    Mobile no      :%.f\n",customer.mobile_no);
+	   printf("    Account number :%d\n",customer.acct_no);
+	   printf("    Street         :%s\n",customer.street);
+	   printf("    City           :%s\n",customer.city);
+	   printf("    Old balance    :%.2f\n",customer.oldbalance);
+	   printf("    Current payment:%.2f\n",customer.payment);
+	   printf("    New balance    :%.2f\n",customer.newbalance);
+	   printf("    Payment date   :%d/%d/%d\n\n",customer.lastpayment.month,customer.lastpayment.day,customer.lastpayment.year);
+	   printf("    Account status :");
+	   switch(customer.acct_type)
+	      {
+	      case 'C':
+		 printf("CURRENT\n\n");
+		 break;
+	      case 'O':
+		 printf("OVERDUE\n\n");
+		 break;
+	      case 'D':
+		 printf("DELINQUENT\n\n");
+		 break;
+	      default:
+		 printf("ERROR\\n\n");
+	      }
+	      return;
+	   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
