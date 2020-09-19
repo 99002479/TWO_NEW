bc14acfaf75d03f20dcfed5eb7a1cdb66075fc7b
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-19 10:46:54 +0530

    Update test_sum.c

--- Code/test/test_sum.c
+++ Code/test/test_sum.c
@@ -9,9 +9,12 @@
 	{
   		TEST_ASSERT_EQUAL(950, checkbalance(1000, 50));
   		TEST_ASSERT_EQUAL(-50, checkbalance(100, 150));
+		TEST_ASSERT_EQUAL(-150, checkbalance(-50, 100));
+		TEST_ASSERT_EQUAL(-50, checkbalance(-50, 0));
+		TEST_ASSERT_EQUAL(100, checkbalance(100, 0));
 	}
 
-	void test_balance(void)
+	void test_account(void)
 	{
   		TEST_ASSERT_EQUAL(1, getaccounttype(1000, 50));
   		TEST_ASSERT_EQUAL(0, getaccounttype(100, 150));
