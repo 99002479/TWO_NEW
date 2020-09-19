388b11175316824ec570e8e1830f612d7cc92535
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-19 10:41:28 +0530

    Update test_sum.c

--- Code/test/test_sum.c
+++ Code/test/test_sum.c
@@ -11,11 +11,20 @@
   		TEST_ASSERT_EQUAL(-50, checkbalance(100, 150));
 	}
 
+	void test_balance(void)
+	{
+  		TEST_ASSERT_EQUAL(1, getaccounttype(1000, 50));
+  		TEST_ASSERT_EQUAL(0, getaccounttype(100, 150));
+	}
+
+
 	int test_main(void)
 	{
 	  	UNITY_BEGIN();
   	
 	  	RUN_TEST(test_balance);
+		
+		RUN_TEST(test_account);
 
   		return UNITY_END();
 	}
